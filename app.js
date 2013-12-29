e13899eadda8f83869645e7bd15918a5d34c7a8e
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2013-12-29 21:14:17 +0000

    simplified boidjs file server

--- app.js
+++ app.js
@@ -10,26 +10,7 @@ http.createServer(function (request, response) {
     var uri = url.parse(request.url).pathname;
     var filename = path.join(process.cwd(), uri);
     
-    path.exists(filename, function(exists) {
-    	if(!exists) {
-    		response.writeHead(404, {"Content-Type": "text/plain"});
-    		response.write("404 Not Found\n");
-    		response.end();
-    		return;
-    	}
-    	
-    	fs.readFile(filename, "binary", function(err, file) {
-    		if(err) {
-    			response.writeHead(500, {"Content-Type": "text/plain"});
-    			response.write(err + "\n");
-    			response.end();
-    			return;
-    		}
-    		
-    		response.writeHead(200);
-    		response.end(file, "binary");
-    	});
-    });
+    fs.createReadStream(filename).pipe(response);
 
 }).listen(8080, function () {
     console.log('Access Boids simulation at http://localhost:8080/index.html');
