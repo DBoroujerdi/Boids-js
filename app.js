734627f95945ed94c84c2806aadc2928ac801d65
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2014-02-13 13:31:33 +0000

    fixed node server to prevent crashing

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ http.createServer(function (request, response) {
     var uri = url.parse(request.url).pathname;
     var filename = path.join(process.cwd(), uri);
     
-    fs.createReadStream(filename).pipe(response);
+    var rs = fs.createReadStream(filename);
+
+    rs.on('data', function(data) {
+	response.write(data);
+    });
+    rs.on('close', function() {
+	response.end();
+    });
+    rs.on('error', function(error) {
+	console.log('error:' + error);
+    });
 
 }).listen(8080, function () {
     console.log('Access Boids simulation at http://localhost:8080/index.html');
